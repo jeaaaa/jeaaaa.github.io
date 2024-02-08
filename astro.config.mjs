a19654423e76967039a5b0e2e4f9ffb3b168047a
jea <jea@mail.cc>
2024-02-08 16:52:57 +0800

    feat: inti

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://jeaaaa.github.io/astro-start/',
   integrations: [mdx(), sitemap(), react(), tailwind({
     applyBaseStyles: false,
   })]
