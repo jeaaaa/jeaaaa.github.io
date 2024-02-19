06ecd4a61f66bf97eb5416135ccae5ccba4a78ce
jea <jea@mail.cc>
2024-02-19 17:30:20 +0800

    feat: menubar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.acfun.wiki/',
+  site: 'https://blog.acfun.online/',
   integrations: [mdx(), sitemap(), react(), tailwind({
     applyBaseStyles: false,
   })]
